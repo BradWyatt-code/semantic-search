5b934c96451aa93c1c4fbf1743927eb720dc60c8
Brad Wyatt <bew018@gmail.com>
2025-12-23 14:48:07 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -279,5 +279,9 @@ async def semantic_search(
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+8000)
